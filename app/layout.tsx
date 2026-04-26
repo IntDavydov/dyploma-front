bddfb4f9b1d96c240ee029c38d5a1bc5bad90a61
artem davydov <successful0506@gmail.com>
2026-04-26 14:55:05 +0300

    fix: light theme + magnifying

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
     >
       <body className="min-h-full flex flex-col bg-background text-foreground font-sans">
         <GoogleOAuthProvider clientId={googleClientId}>
